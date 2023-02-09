9b518e43dbaf159075af3bff0a2f926eaab8006f
spyroot <spyroot@gmail.com>
2023-02-09 15:31:33 +0400

    generic downloader

--- docker_images/download_docker_imgs.sh
+++ docker_images/download_docker_imgs.sh
@@ -29,18 +29,81 @@ if is_not_empty "$DEFAULT_IMAGE_NAME" && is_not_empty "$BUILD_HOST"; then
   scp vmware@"$BUILD_HOST":/home/vmware/$DEFAULT_IMAGE_NAME $DEFAULT_IMAGE_NAME
 fi
 
+#function is_ubuntu_os() {
+#  local -r ver="$1"
+#  grep -q "Ubuntu $ver" /etc/*release &>/dev/null
+#}
+
+function is_mac_os {
+  uname -a|grep Darwin &>/dev/null
+}
+
+# function generate random number on macOS
+# usage
+# my_rand_num
+# rand_num=$(mac_random "my_rand_num")
+# echo "my_rand_num"
+function mac_random() {
+  local num_times=1
+  local _result_rand=$1
+  local i="0"
+  while [[ $i -lt $num_times ]]; do
+    #LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= </dev/random | head -c 20 | xargs
+    LC_CTYPE=C tr -dc A-Za-z0-9 </dev/random | head -c 20 | xargs
+    i=$((i + 1))
+  done
+
+  if [[ "$_result_rand" ]]; then
+    eval "$_result_rand"="'$i'"
+  fi
+}
+
+# function generate random
+# in case mac it read from /dev/random
+# in all other cases /proc/sys/kernel/random/uuid
+# usage
+# my_random_seq
+# rand_num=$(random_seq "my_random_seq")
+function random_seq() {
+  local _result_rand_num=$1
+  local rand_num
+  if is_mac_os; then
+    rand_num=$(mac_random "$rand_num")
+  else
+    while IFS= read -r rand_num; do echo "$rand_num"| head -c 5; done < /proc/sys/kernel/random/uuid
+  fi
+  if [[ "$_result_rand_num" ]]; then
+    eval "$_result_rand_num"="'$rand_num'"
+  else
+    echo "$rand_num"
+  fi
+}
+
+# function fetch file
+# User download "http://my_http/download.me" "download.me"
+# arg user and password optional
 function download() {
   local url=$1
-  if is_not_empty "$url"; then
-    echo "Downloading images from $url"
-    wget -q -nc "$url"
+  local file_name=$2
+  local user=$3
+  local password=$4
+  local rand_seq
+  if is_not_empty "$url" && is_not_empty "$file_name"; then
+    random_seq "rand_seq"
+    echo "Downloading images from $url, log file $rand_seq.log"
+    if is_not_empty "$user"; then
+        wget --user="$user" --password="$password" -b -nc "$url" -o "$rand_seq.log"
+    else
+      wget -b -nc "$url" -o -O "$file_name".zip "$rand_seq.log"
+    fi
   fi
 }
 
 if is_not_empty "$BUILD_WEB_HOST"; then
+  rm -rf ./*.log
   echo "Downloading images from $BUILD_WEB_HOST"
-  download http://"$BUILD_WEB_HOST"/testnf/testnf-du-flexran-base210.tar
-  download http://"$BUILD_WEB_HOST"/testnf/testnf-du-flexran-base210.tar
-  download http://"$BUILD_WEB_HOST"/testnf/testnf-du-lite210.tar
-  download http://"$BUILD_WEB_HOST"/testnf/testnf-du-lite220.tar
+  download "$BUILD_WEB_HOST"/testnf/testnf-du-flexran-base210.tar "testnf-du-flexran-base210.tar"
+  download "$BUILD_WEB_HOST"/testnf/testnf-du-flexran-base210.tar "testnf-du-flexran-base210.tar"
+  download "$BUILD_WEB_HOST"/testnf/testnf-du-lite210.tar "testnf-du-lite210.tar"
+  download "$BUILD_WEB_HOST"/testnf/testnf-du-lite220.tar "testnf-du-lite220.tar"
 fi
