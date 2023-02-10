bd4c6b5c56fd43a7cf6acba004825fd867154de9
spyroot <spyroot@gmail.com>
2023-02-10 06:09:14 +0400

    fixed issue with noarch

--- build_paraller_boot.sh
+++ build_paraller_boot.sh
@@ -77,8 +77,8 @@ fi
 pip --quiet install idrac_ctl -U &> /dev/null
 pip --quiet pygments tqdm requests -U &> /dev/null
 
-rm rf $DEFAULT_LOCATION_MOVE/"$DEFAULT_IMAGE_NAME"
 echo "Removing  $DEFAULT_LOCATION_MOVE/$DEFAULT_IMAGE_NAME"
+rm -rf $DEFAULT_LOCATION_MOVE/"$DEFAULT_IMAGE_NAME"
 
 ## build-iso.sh generates ph4-rt-refresh_adj.iso
 src_hash=$(md5sum "$DEFAULT_IMAGE_NAME")
