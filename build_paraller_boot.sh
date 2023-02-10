13f688ab8e37523b21954844d7f7173476bc9cef
spyroot <spyroot@gmail.com>
2023-02-10 21:57:48 +0400

    test

--- build_paraller_boot.sh
+++ build_paraller_boot.sh
@@ -139,26 +139,26 @@ function main() {
   fi
 
   install_idrac_ctl
-
-  # by a default we always do clean build
-  if [[ -z "$IDRAC_IPS" ]]; then
-    log "IDRAC_IPS variable is empty, it must store either IP address or list comma seperated."
-    exit 99
-  else
-    log "Using $IDRAC_IPS."
-  fi
-
-  # first trim all whitespace and then iterate.
-  idrac_ip_list=$(trim "$IDRAC_IPS")
-  IFS=',' read -ra IDRAC_IP_ADDR <<<"$idrac_ip_list"
-  for IDRAC_HOST in "${IDRAC_IP_ADDR[@]}"; do
-    local addr
-    addr=$(trim "$IDRAC_HOST")
-
-    adjust_bios_if_needed "$addr"
-
-    boot_host "$addr"
-  done
+#
+#  # by a default we always do clean build
+#  if [[ -z "$IDRAC_IPS" ]]; then
+#    log "IDRAC_IPS variable is empty, it must store either IP address or list comma seperated."
+#    exit 99
+#  else
+#    log "Using $IDRAC_IPS."
+#  fi
+#
+#  # first trim all whitespace and then iterate.
+#  idrac_ip_list=$(trim "$IDRAC_IPS")
+#  IFS=',' read -ra IDRAC_IP_ADDR <<<"$idrac_ip_list"
+#  for IDRAC_HOST in "${IDRAC_IP_ADDR[@]}"; do
+#    local addr
+#    addr=$(trim "$IDRAC_HOST")
+#
+#    adjust_bios_if_needed "$addr"
+#
+#    boot_host "$addr"
+#  done
 }
 
 main
