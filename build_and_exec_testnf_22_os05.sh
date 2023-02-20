3ac347de492b11318110fed14d48c71b5df3cb31
spyroot <spyroot@gmail.com>
2023-02-20 23:37:41 +0100

    elevating RT ISO for 5.0

--- build_and_exec_testnf_22_os05.sh
+++ build_and_exec_testnf_22_os05.sh
@@ -4,6 +4,7 @@
 #
 # spyroot@gmail.com
 # Author Mustafa Bayramov
-PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" ./build_and_exec.sh
-PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" ./build_iso.sh
-PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" ./build_in_parallel_boot.sh
+PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" DPDK_VER="22.11" ./build_and_exec.sh
+PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" DPDK_VER="22.11" ./build_iso.sh
+PHOTON_5_X86=yes BUILD_TYPE="offline_testnf_os5_flex22" DPDK_VER="22.11" ./build_in_parallel_boot.sh
+
