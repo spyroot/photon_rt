a9bf19dc4e3cd654146416c241c457e7daeff050
spyroot <spyroot@gmail.com>
2023-02-21 07:53:56 +0100

    issue with docker

--- build_and_exec.sh
+++ build_and_exec.sh
@@ -390,7 +390,6 @@ function git_clone() {
           repo_tmp_dir="$git_repos_dir/$repo_name"
           echo "Compressing $repo_tmp_dir"
           cd "$DEFAULT_GIT_DIR" || exit; tar -zcvf "$repo_name".tar.gz "$repo_name"
-          mv "$repo_name".tar.gz "$DEFAULT_GIT_DIR/"; cd "$DEFAULT_GIT_DIR" || exit
       fi
     done
     #rm -rf "$git_repos_dir"
